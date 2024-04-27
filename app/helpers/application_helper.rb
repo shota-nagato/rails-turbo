57ba808cfe80da4a7085013e5838aad8680e07cb
shota-nagato <shota.nagato@gmail.com>
2024-04-27 10:18:55 +0900

    見た目を整える

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def sidebar_link_to(path, emoji, text)
+    classes = %w[my-1 text-white font-bold flex rounded py-2]
+    classes << "bg-blue-600" if current_page?(path)
+
+    link_to(path, class: classes) do
+      tag.span(class: "mx-2") { emoji } + tag.span { text }
+    end
+  end
 end
